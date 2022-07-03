2268ce64a7e95ed424f7e6ec97973101ae00bb90
Vinicius Oliveira <50337315+vinicius9141@users.noreply.github.com>
2022-07-02 22:14:06 -0300

    app alcool ou gasolina

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public resultado = 'Resultado';
+  public precoAlcool: number;
+  public precoGasolina: number;
+  public resulPreco: number;
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  public calcAlcoolGas(): void {
+    if (this.precoAlcool && this.precoGasolina) {
+      this.resulPreco = this.precoGasolina / this.precoAlcool;
+    } else {
+      this.resultado = 'Preencha os campos corretamente';
+    }
+
+    if (this.resulPreco >= 0.7) {
+      this.resultado = 'Melhor Utilizar Gasolina';
+    } else {
+      this.resultado = 'Melhor Utilizar Alcool';
+    }
+  }
+
+  async presentAlert() {
+    this.calcAlcoolGas();
+    const alert = await this.alertController.create({
+      header: 'Resultado com base nos preços informados',
+      subHeader: 'Resultado: ' + this.resulPreco,
+      message: this.resultado,
+      buttons: ['Esta bem :)'],
+    });
+
+    await alert.present();
+  }
 }
